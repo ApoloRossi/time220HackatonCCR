c75891500021680f29680d09b6dd9a4034294fee
apolorossi <apolorossi@gmail.com>
2021-01-16 02:33:28 -0300

    feat: added extension functions to deal with dates.

---
+++ app/src/main/java/com/apolo/arteapp/util/ExtensionsFunctions.kt
@@ -0,0 +1,30 @@
+package com.apolo.arteapp.util
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun String.getYear(): Int {
+    try {
+        SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).parse(this)?.let { parsedDate ->
+            return Calendar.getInstance().let { calendar ->
+                calendar.time = parsedDate
+                calendar
+                    .get(Calendar.YEAR)
+            }
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+
+    return -1
+}
+
+fun String.formatDate() : String {
+    try {
+        return SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).parse(this))
+    }catch (e : java.lang.Exception) {
+        e.printStackTrace()
+    }
+
+    return this
+}
